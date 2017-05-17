9c11010b962be765a6bb1cfcb33dd75cf39f2170
ikgithub81 <INKorpas@student.naperville203.org>
2017-05-17 15:12:27 -0500

    Worked on main and background

---
+++ escape/Background.java
@@ -0,0 +1,20 @@
+import java.applet.*;
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+
+/**
+ * Make the background a certain color that I choose
+ * The background is just a filled rectangle because I need to change the rectangle color as the program runs
+ * */
+public class Background
+{
+
+public void drawBackground(Graphics page) {
+
+        ImageIcon pic = new ImageIcon("//images.Road");
+        page.drawImage(pic.getImage(), 0, 0, this);
+    }
+
+
+}
--- escape/Main.java
+++ escape/Main.java
@@ -1,11 +1,25 @@
 //main class to run applet
 
-public class Main
+import java.applet.*;
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+
+public class Main extends Applet
 {
 
 public static void main(String[]args)
 {
 
+	APPLET_WIDTH = 700;
+	APPLET_HEIGHT = 700;
+
+	setSize (APPLET_WIDTH, APPLET_HEIGHT);
+
+
+	Background background = new Background();
+
+	background.drawBackground();
 
 
 
