8b05dead86ea71bf8c6bd1be73371500d137a0b1
Meghan <MCSwain@student.naperville203.org>
2017-05-19 15:11:33 -0500

    worked on end and opening screen

--- escape/screen.java
+++ escape/screen.java
@@ -1,13 +1,42 @@
 //Meghan Swain
 //title and end screen
+import java.util.*;
+import java.lang.*;
+import javax.swing.*;
+import java.awt.*;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
 public class screen
 {
+	private JPanel myPanel;
+    private ImageIcon opening = new ImageIcon("images/open.JPEG");
+    private ImageIcon closing = new ImageIcon("images/close.JPEG");
 
-	public screen
+	public screen(JPanel theirPanel)
 	{
-
-
+		myPanel = theirPanel;
 
 	}//ends constructor
+
+    public void displayTitle(Graphics g)
+    {
+        g.drawImage(opening.getImage(), 0, 0, myPanel);
+    }
+
+    public void displayGameOver(Graphics g)
+    {
+        g.drawImage(closing.getImage(), 0, 0, myPanel);
+    }
+
+
+ //   @Override
+//    protected void paintComponent(Graphics g) {
+//        super.paintComponent(g);
+//        g.drawImage(image, 0, 0, this);
+
+
+
+
+
 }//ends screen
