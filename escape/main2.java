692f645db36092cb816a6b364312d550bec00ddd
Meghan <MCSwain@student.naperville203.org>
2017-05-22 15:10:38 -0500

    update

--- escape/main2.java
+++ escape/main2.java
@@ -5,13 +5,115 @@ import javax.swing.*;
 import java.util.*;
 
 
+public class main2 extends Applet
+{
+    //constants for window size
+    private final int APPLET_WIDTH = 700;
+    private final int APPLET_HEIGHT = 700;
 
-public class main2 extends JFrame{
+    //DOUBLE BUFFERING - Variables
+    private Image i;
+    private Graphics doubleG;
+    int myPixelCount, myflag;
+
+
+    public void init()
+    {
+        setSize (APPLET_WIDTH, APPLET_HEIGHT);    //set up applet window
+        myPixelCount = 0;
+        myflag = 1;
+	}
+
+
+
+    //DOUBLE BUFFERING - Method
+
+    public void update(Graphics g){
+
+        //creates an image if there isn't one
+        if (i == null){
+            i = createImage(APPLET_WIDTH,APPLET_HEIGHT);
+
+            doubleG = i.getGraphics();
+        }
+
+        //doubleG.setColor(getBackground());    //i don't use a background color
+
+        doubleG.fillRect(0,0,APPLET_WIDTH,APPLET_HEIGHT);   //makes the image it is drawn on
+
+        paint(doubleG);  //paints to that image
+
+        g.drawImage(i, 0, 0, this);  //draws the image on the official thing
+
+    }
+
+
+    public void paint (Graphics page)
+    {
+		ImageIcon road = new ImageIcon("image//Road.PNG");
+		page.drawImage(road.getImage(),0,0,null);
+
+
+
+
+        try {
+            Thread.sleep(17);   //pauses for x milliseconds
+        }
+        catch(InterruptedException e){}
+
+
+        repaint();   //calls paint and allows for animation, essentially makes this paint method an infinite loop
+
+    }
+
+
+
+
+}
+/*
+
+
+
+
+
+JFrame frame = new JFrame();
+		frame.setSize(700,700);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Crossy Avenuez");
+        frame.setLocationRelativeTo(null);
+
+		ImageIcon road = new ImageIcon("image\\Road.JPEG");
+        JLabel imageLabel = new JLabel(road);
+        frame.add(imageLabel);
+
+		frame.setVisible(true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public static void main(String[] args){
+		new main2();}
+
+
 /*
 	//Graphics myGraphic = new Graphics();
 
-	public static void main(String[] args){
-		new main2();
+
 		}
 
 
@@ -27,14 +129,7 @@ public class main2 extends JFrame{
 		//(The panel object -  a panel surrounds the components and is in the frame)
 		JPanel thePanel = new JPanel();
 
-*/
 
-/**
- * Meghan Swain
- * 1/20/17
- * Software Engineering Challenge
- * Runs all the methods and creates the image
- */
 
 
     //constants for window size
@@ -81,55 +176,19 @@ public class main2 extends JFrame{
 
     public void paint (Graphics page)
        {
-		Image road = new Image("image/Road.PNG");
-		page.drawImage(Image img,int x, int y,ImageObserver observer);
+		ImageIcon road = new ImageIcon("image\\Road.PNG");
+		page.drawImage(road.getImage(),0, 0,null);
 
 
-        //draws a black rectangle for a road
-        page.setColor(Color.BLACK);
-        page.fillRect(0, APPLET_HEIGHT - 150, APPLET_WIDTH, 100);
-
-        //white lane seperators. For loop allows me to make them dashes
-        page.setColor(Color.WHITE);
-        for (int dash = 0; dash * 30 <= APPLET_WIDTH + 1; dash++)
-        {
-            page.fillRect(0 + 30*dash, APPLET_HEIGHT - 126, 20, 3); //makes each dash 20 long, 3 high
-            page.fillRect(0 + 30*dash, APPLET_HEIGHT - 74, 20, 3);
-
-        }
 
 
-        //yellow center lines
-        page.setColor(Color.YELLOW);
-        page.fillRect(0, APPLET_HEIGHT - 104, APPLET_WIDTH, 2);  //two yellow lines one on top of other
-        page.fillRect(0, APPLET_HEIGHT - 98, APPLET_WIDTH, 2);
 
 
-        //draws the sky. Count is to see how many pixels we have passed
-        mySky.draw(page, myPixelCount, myflag);
-        myPixelCount++;
-        if (myPixelCount == APPLET_WIDTH +100){
-            myPixelCount = 0;
-            myflag *= -1;
-        }
 
-        //adds all of the bulidings to the screen
-        b1.draw (page);
-        b2.draw (page);
-        b3.draw (page);
-        b4.draw (page);
-        b5.draw (page);
-        b6.draw (page);
-        b7.draw (page);
-        b8.draw (page);
-        b9.draw (page);
-        b10.draw (page);
-
-
-        try {
-            Thread.sleep(17);   //pauses for x milliseconds
-        }
-        catch(InterruptedException e){}
+        //try {
+        //    Thread.sleep(17);   //pauses for x milliseconds
+        //}
+        //catch(InterruptedException e){}
 
 
         repaint();   //calls paint and allows for animation, essentially makes this paint method an infinite loop
@@ -140,3 +199,4 @@ public class main2 extends JFrame{
 
 
 }
+*/
